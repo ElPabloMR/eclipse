6879e251c64dd9ba975b51208416964f23bf1497
ElPabloMR <pablomartinruiz1@gmail.com>
2024-03-14 14:10:25 +0100

    clase personas

---
+++ Personas/Model/Persona.java
@@ -0,0 +1,66 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persona {
+
+    /**
+     * Default constructor
+     */
+    public Persona() {
+    }
+
+    /**
+     * 
+     */
+    private int DNI;
+
+    /**
+     * 
+     */
+    private String nombre;
+
+    /**
+     * 
+     */
+    private String Apellido1;
+
+    /**
+     * 
+     */
+    private String Apellido2;
+
+    /**
+     * 
+     */
+    private int Edad = 0;
+
+    /**
+     * @param dni 
+     * @param nombre 
+     * @param apellido1 
+     * @param apellido2 
+     * @param edad
+     */
+    public void Persona(String dni, void nombre, String apellido1, String apellido2, int edad) {
+        // TODO implement here
+    }
+
+    /**
+     * @param DNI
+     */
+    public void setDNI(String DNI) {
+        // TODO implement here
+    }
+
+    /**
+     * @return
+     */
+    public null getDNI() {
+        // TODO implement here
+        return null;
+    }
+
+}
