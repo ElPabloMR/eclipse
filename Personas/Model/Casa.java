27d954e38c8f190602c72e20748fb5b3f4b6fd70
ElPabloMR <pablomartinruiz1@gmail.com>
2024-03-21 09:51:46 +0100

    Clase Casa creada

--- Personas/Model/Casa.java
+++ Personas/Model/Casa.java
@@ -2,41 +2,30 @@
 import java.util.*;
 
 /**
- * 
+ * Representa una casa del mundo real.
  */
 public class Casa {
-
-    /**
-     * Default constructor
-     */
+    private String direccion;
+    private int numHabitaciones;
+    private int metrosCuadrados;
     public Casa() {
+    	/**
+         * Constructor que recibe como parametros la direccion, el numero de habitacion y los metros cuadrados
+         * 
+         * @param direccion  La direccion de la casa
+         * @param numHabitaciones  El numero de habitaciones de la casa
+         * @param metrosCuadrados Los metros cuadrados de la casa
+         */
+    }
+    public Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
+    	this.direccion=direccion;
+    	this.numHabitaciones=numHabitaciones;
+    	this.metrosCuadrados=metrosCuadrados;
     }
 
-    /**
-     * 
-     */
-    private String direccion;
 
-    /**
-     * 
-     */
-    private int numHabitaciones;
-
-    /**
-     * 
-     */
-    private int metrosCuadrados;
-
-    /**
-     * 
-     */
     public void Attribute1;
 
-    /**
-     * @param direccion 
-     * @param numHabitaciones 
-     * @param metrosCuadrados
-     */
     public void Casa(String direccion, int numHabitaciones, int metrosCuadrados) {
         // TODO implement here
     }
